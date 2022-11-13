27d5ae4f5d6f6e5441ce4fdd61fc67785c13c32a
yaseengem <yaseengem@gmail.com>
2022-11-14 00:31:22 +1100

    Create a bar with lefft three line menu, right side search and profile.. Just like google app bar. Fixes #7

--- lib/src/shared/theme.dart
+++ lib/src/shared/theme.dart
@@ -6,7 +6,7 @@ var lightTheme = ThemeData.light();
 enum ThemeType { Light, Dark }
 
 class ThemeModel extends ChangeNotifier {
-  ThemeData currentTheme = darkTheme;
+  ThemeData currentTheme = lightTheme;
   ThemeType _themeType = ThemeType.Dark;
 
   toggleTheme() {
