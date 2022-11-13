53a5957eef24582625a05e981aed42f340a804d3
yaseengem <yaseengem@gmail.com>
2022-11-13 23:02:01 +1100

    Test

---
+++ lib/src/pages/second.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import '../classes/objects.dart';
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final args = ModalRoute.of(context)!.settings.arguments as Course;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const SelectableText('Second Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: SelectableText(args.name),
+        ),
+      ),
+    );
+  }
+}
